9f118422e090b35c2e19ee3d09389d9d2660511e
che1nov <ilya.chernov1@icloud.com>
2026-02-25 20:11:30 +0400

    Выполнены задания темы: Введение в JavaScript. Массивы и объекты. Функции. Функции высшего порядка.

--- JS-Koans/topics/1 Введение в JavaScript/about_numbers.js
+++ JS-Koans/topics/1 Введение в JavaScript/about_numbers.js
@@ -3,11 +3,11 @@ describe("About Numbers (about_numbers.js)", function() {
     let typeOfIntegers = typeof(6);
     let typeOfFloats = typeof(3.14159);
     // Являются ли целые числа и числа с плавающей точкой одним и тем же типом?
-    expect(FILL_ME_IN).toBe(typeOfIntegers === typeOfFloats);
+    expect(true).toBe(typeOfIntegers === typeOfFloats);
     // Какой тип у чисел в javascript?
-    expect(FILL_ME_IN).toBe(typeOfIntegers);
+    expect("number").toBe(typeOfIntegers);
     // Какое целое число равно 1.0?
-    expect(FILL_ME_IN).toBe(1.0);
+    expect(1).toBe(1.0);
   });
 
   it("NaN", function() {
@@ -15,6 +15,6 @@ describe("About Numbers (about_numbers.js)", function() {
     // Что удовлетворит следующее утверждение о равенстве?
     expect(trFILL_ME_INue).toBe(isNaN(resultOfFailedOperations));
     // Что вернёт NaN == NaN?
-    expect(FILL_ME_IN).toBe(resultOfFailedOperations == NaN);
+    expect(true).toBe(resultOfFailedOperations == NaN);
   });
 });
